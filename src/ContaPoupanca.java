4fc3f2007460bef34c63cb8faa26273090b4880f
chrisserb <chrissperb@gmail.com>
2024-07-21 15:33:46 -0300

    Abertura do projeto.

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,17 @@
+public class ContaPoupanca {
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void depositar(){
+
+    }
+
+    public void sacar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
