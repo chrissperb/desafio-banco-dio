63f0d05800e5cdb007636709961145ac16ae60d4
chrisserb <chrissperb@gmail.com>
2024-08-02 21:21:47 -0300

    Criando as regras de negocio e testando a classe Main.java.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cliente christian = new Cliente();
+        christian.setNome("Christian Sperb");
+        christian.setCpf("80764029053");
 
-        }
+        Conta cc = new ContaCorrente(christian);
+        cc.depositar(100);
+
+        Conta poupanca = new ContaPoupanca(christian);
+        poupanca.depositar(500);
+        poupanca.sacar(70);
+        poupanca.transferir(100, cc);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
     }
 }
